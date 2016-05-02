8b81f89ebb4a91750893ee3dca543c1f73d8fa67
Sarah Spall <sspall10@gmail.com>
2016-05-01 23:05:59 -0600

    using racket generic graph library

--- semantics.rkt
+++ semantics.rkt
@@ -1,17 +1,99 @@
 #lang racket
 
+(require graph)
 (provide (all-defined-out))
-
-(struct graph (nodes)
-  #:transparent)
-
-(define create-graph (lambda (n) (graph n)))
 
 (struct node (id content edges) #:transparent)
 
 (struct edge (node1 node2 [directed #:auto])
   #:auto-value #f
-  #:transparent) ;; add a direction. if direction it is always from node1 to node2, otherwise undirected.
+  #:transparent)
+
+(struct graph (nodes edges)
+  #:methods gen:graph
+  [(define (has-vertex? g v)
+     (define helper
+       (lambda (nodes)
+         (cond
+           [(empty? nodes)
+            #f]
+           [(equal? (node-id v) (node-id (car nodes)))
+            #t]
+           [else
+            (helper (cdr nodes))])))
+     (helper (graph-nodes g)))
+   
+   (define (has-edge? g u v)
+     (define helper
+       (lambda (e)
+         (or (and (vertex=? g (edge-node1 e) v)
+                  (vertex=? g (edge-node2 e) u))
+             (and (vertex=? g (edge-node1 e) u)
+                  (vertex=? g (edge-node2 e) v)))))
+     (and (ormap helper (node-edges u))
+          (ormap helper (node-edges v))))
+
+   (define (vertex=? g u v)
+     (equal? (node-id u) (node-id v)))
+   
+   (define (add-vertex! g v)
+     (displayln "this function is not implemented"))
+   
+   (define (remove-vertex! g v) (displayln "this function is not implemented"))
+   (define (add-edge! g u v [weight 1]) (displayln "this function is not implemented"))
+   (define (add-directed-edge! g u v [weight 1]) (displayln "this function is not implemented"))
+   (define (remove-edge! g u v) (displayln "this function is not implemented"))
+   (define (remove-directed-edge! g u v) (displayln "this function is not implemented"))
+   
+   (define (get-vertices g)
+     (graph-nodes g))
+   
+   (define (in-vertices g)
+     (graph-nodes g))
+   
+   (define (get-neighbors g v)
+     (define helper
+       (lambda (e)
+         (cond
+           [(equal? (node-id (edge-node1 e))
+                    (node-id v))
+            (edge-node2 e)]
+           [else
+            (edge-node1 e)])))
+     (map helper (node-edges v)))
+   
+   (define (in-neighbors g v)
+     (get-neighbors g v))
+   
+   (define (get-edges g)
+     (graph-edges g))
+   
+   (define (in-edges g)
+     (graph-edges g))
+   
+   (define (edge-weight g u v)
+     (define helper
+       (lambda (edges)
+         (cond
+           [(empty? edges)
+            +inf.0]
+           [(or (and (vertex=? g (edge-node1 (car edges)) v)
+                      (vertex=? g (edge-node2 (car edges)) u))
+                 (and (vertex=? g (edge-node1 (car edges)) u)
+                      (vertex=? g (edge-node2 (car edges)) v)))
+            1]
+           [else
+            (helper (cdr edges))])))
+     (helper (graph-edges g)))
+   
+   (define (transpose g) g)
+   
+   (define (graph-copy g) g)]
+  #:transparent)
+
+
+(define create-graph (lambda (n) (graph n)))
+ ;; add a direction. if direction it is always from node1 to node2, otherwise undirected.
 
 (define add-node
   (lambda (n g)
