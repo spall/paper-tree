f20cd3a16da89d091383a6d770dcb4eb2e15f82a
Sarah Spall <sspall10@gmail.com>
2016-05-03 16:41:10 -0600

    removed weight from edge added function to convert edge to list of nodes change graph to my-graph added write-graph-to-dot function

--- semantics.rkt
+++ semantics.rkt
@@ -1,41 +1,48 @@
 #lang racket
 
 (require graph)
-(provide (all-defined-out))
+;; all defined out doesn't work for functions defined in a structure? :(
+(provide (struct-out my-graph)
+         (all-defined-out)
+         graphviz)
 
 (struct node (id content) #:transparent)
 
-(struct edge (node1 node2 weight [directed #:auto])
+(struct edge (node1 node2 [directed #:auto])
   #:auto-value #f
   #:transparent)
 
 (define (is-edge? g u v e)
-         (or (and (vertex=? g (edge-node1 e) v)
-                  (vertex=? g (edge-node2 e) u))
-             (and (vertex=? g (edge-node1 e) u)
-                  (vertex=? g (edge-node2 e) v))))
+  (or (and (vertex=? g (edge-node1 e) v)
+           (vertex=? g (edge-node2 e) u))
+      (and (vertex=? g (edge-node1 e) u)
+           (vertex=? g (edge-node2 e) v))))
 
-(struct graph (nodes edges)
+;; this function converts edges because graph generics expects edges to be lists. but afaik does not state this in docs!
+(define (convert-edge e)
+  (list (edge-node1 e) (edge-node2 e)))
+  
+(struct my-graph (nodes edges)
   #:methods gen:graph
   [(define (has-vertex? g v)
      (define (helper nodes)
          (cond
            [(empty? nodes)
             #f]
-           [(equal? (node-id v) (node-id (car nodes)))
+           [(vertex=? g v (car nodes))
             #t]
            [else
             (helper (cdr nodes))]))
-     (helper (graph-nodes g)))
+     (helper (my-graph-nodes g)))
    
    (define (has-edge? g u v)
-     (ormap (curry is-edge? g u v) (graph-edges g)))
+     (ormap (curry is-edge? g u v) (my-graph-edges g)))
 
    (define (vertex=? g u v)
      (equal? (node-id u) (node-id v)))
    
    (define (add-vertex! g v)
-     (set-graph-nodes! g (append (graph-nodes g) (list v))))
+     (set-my-graph-nodes! g (append (my-graph-nodes g) (list v))))
    
    (define (remove-vertex! g v)
      (define (helper nodes)
@@ -57,8 +64,8 @@
           (helper (cdr edges))]
          [else
           (cons (car edges) (remove-edge (cdr edges)))]))
-     (set-graph-nodes! g (helper (graph-nodes g)))
-     (set-graph-edges! g (remove-edge (graph-edges g))))
+     (begin (set-my-graph-nodes! g (helper (my-graph-nodes g)))
+            (set-my-graph-edges! g (remove-edge (my-graph-edges g)))))
 
    (define (rename-vertex! g old new)
      (define (helper nodes)
@@ -72,24 +79,24 @@
      (define (update-edge e)
        (cond
          [(vertex=? g (edge-node1 e) old)
-          (edge new (edge-node2 e) (edge-weight e) (edge-directed e))]
+          (edge new (edge-node2 e) (edge-directed e))]
          [(vertex=? g (edge-node2 e) old)
-          (edge (edge-node1 e) new (edge-weight e) (edge-directed e))]
+          (edge (edge-node1 e) new  (edge-directed e))]
          [else
           e]))
      (if (has-vertex? g new)
-         (error 'rename-vertex! "Error: graph already contains new vertex")
+         (error 'rename-vertex! "graph already contains new vertex")
          (begin
-           (set-graph-nodes! g (helper (graph-nodes g)))
-           (set-graph-edges! g (map update-edge (graph-edges g))))))
+           (set-my-graph-nodes! g (helper (my-graph-nodes g)))
+           (set-my-graph-edges! g (map update-edge (my-graph-edges g))))))
    
    (define (add-edge! g u v [weight 1])
      (let ([new-edge (edge u v weight)])
-       (set-graph-edges! g (cons new-edge (graph-edges g)))))
+       (set-my-graph-edges! g (cons new-edge (my-graph-edges g)))))
    
    (define (add-directed-edge! g u v [weight 1])
      (let ([new-edge (edge u v weight #t)])
-       (set-graph-edges! g (cons new-edge (graph-edges g)))))
+       (set-my-graph-edges! g (cons new-edge (my-graph-edges g)))))
    
    (define (remove-edge! g u v)
      (define (helper edges)
@@ -101,7 +108,7 @@
           (cdr edges)]
          [else
           (cons (car edges) (helper (cdr edges)))]))
-     (set-graph-edges! g (helper (graph-edges g))))
+     (set-my-graph-edges! g (helper (my-graph-edges g))))
      
    (define (remove-directed-edge! g u v)
      (define (helper edges)
@@ -113,13 +120,13 @@
           (cdr edges)]
          [else
           (cons (car edges) (helper (cdr edges)))]))
-     (set-graph-edges! g (helper (graph-edges g))))
+     (set-my-graph-edges! g (helper (my-graph-edges g))))
    
    (define (get-vertices g)
-     (graph-nodes g))
+     (my-graph-nodes g))
    
    (define (in-vertices g)
-     (graph-nodes g))
+     (my-graph-nodes g))
    
    (define (get-neighbors g v)
      (define helper
@@ -131,28 +138,19 @@
             (edge-node1 e)]
            [else
             #f])))
-     (filter helper (graph-edges g)))
+     (filter helper (my-graph-edges g)))
    
    (define (in-neighbors g v)
      (get-neighbors g v))
    
    (define (get-edges g)
-     (graph-edges g))
+     (map convert-edge (my-graph-edges g)))
    
    (define (in-edges g)
-     (graph-edges g))
+     (map convert-edge (my-graph-edges g)))
    
    (define (edge-weight g u v)
-     (define helper
-       (lambda (edges)
-         (cond
-           [(empty? edges)
-            +inf.0]
-           [(is-edge? g u v (car edges))
-            (edge-weight (car edges))]
-           [else
-            (helper (cdr edges))])))
-     (helper (graph-edges g)))
+     +inf.0)
    
    (define (transpose g)
      (define (helper edges)
@@ -162,21 +160,57 @@
          [(edge-directed (car edges))
           (edge (edge-node2 (car edges))
                 (edge-node1 (car edges))
-                (edge-weight (car edges))
                 (edge-directed (car edges)))]
          [else
           (cons (car edges) (helper (cdr edges)))]))
-     (let ([new-edges (helper (graph-edges g))])
-       (graph (graph-nodes g) new-edges)))
+     (let ([new-edges (helper (my-graph-edges g))])
+       (my-graph (my-graph-nodes g) new-edges)))
    
-   (define (graph-copy g)
-     (graph (graph-nodes g) (graph-edges g)))
+   (define (my-graph-copy g)
+     (my-graph (my-graph-nodes g) (my-graph-edges g)))
 
    ;; extra functions that are not part of graph generics
    ;; define functional versions of each imperitive function defined by graph
-   (define (add-vertex g v)
-     (let ([new-nodes (append (graph-nodes g) (list v))])
-       (graph new-nodes (graph-edges g))))
+ ]
+  #:mutable
+  #:transparent)
+
+(define find-node
+  (lambda (n g)
+    (define helper
+      (lambda (ns)
+        (cond
+          [(empty? ns)
+           (error 'find-node "could not find node")]
+          [(equal? (node-id n) (node-id (car ns)))
+           (car ns)]
+          [else
+           (helper (cdr ns))])))
+    (helper (my-graph-nodes g))))
+
+(define construct-graph
+  (lambda (nodes need-edge? make-edge)
+    (define helper
+      (lambda (node other-nodes)
+        (cond
+          [(empty? other-nodes)
+           '()]
+          [(need-edge? node (car other-nodes))
+           (cons (make-edge node (car other-nodes))
+                 (helper node (cdr other-nodes)))]
+          [else
+           (helper node (cdr other-nodes))])))
+    (define all-edges
+      (lambda (ns)
+        (if (empty? ns)
+            '()
+            (append (helper (car ns) (cdr ns))
+                    (all-edges (cdr ns))))))
+      (my-graph nodes (all-edges nodes))))
+
+  (define (add-vertex g v)
+     (let ([new-nodes (append (my-graph-nodes g) (list v))])
+       (my-graph new-nodes (my-graph-edges g))))
    
    (define (remove-vertex g v)
      (define (helper nodes)
@@ -198,9 +232,9 @@
           (helper (cdr edges))]
          [else
           (cons (car edges) (local-remove-edge (cdr edges)))]))
-     (let ([new-nodes (helper (graph-nodes g))]
-           [new-edges (local-remove-edge (graph-edges g))])
-       (graph new-nodes new-edges)))
+     (let ([new-nodes (helper (my-graph-nodes g))]
+           [new-edges (local-remove-edge (my-graph-edges g))])
+       (my-graph new-nodes new-edges)))
 
    (define (rename-vertex g old new)
      (define (helper nodes)
@@ -214,24 +248,24 @@
      (define (update-edge e)
        (cond
          [(vertex=? g (edge-node1 e) old)
-          (edge new (edge-node2 e) (edge-weight e) (edge-directed e))]
+          (edge new (edge-node2 e) (edge-directed e))]
          [(vertex=? g (edge-node2 e) old)
-          (edge (edge-node1 e) new (edge-weight e) (edge-directed e))]
+          (edge (edge-node1 e) new (edge-directed e))]
          [else
           e]))
      (if (has-vertex? g new)
          (error 'rename-vertex! "Error: graph already contains new vertex")
-         (let ([new-nodes (helper (graph-nodes g))]
-               [new-edges (map update-edge (graph-edges g))])
-           (graph new-nodes new-edges))))
+         (let ([new-nodes (helper (my-graph-nodes g))]
+               [new-edges (map update-edge (my-graph-edges g))])
+           (my-graph new-nodes new-edges))))
 
    (define (add-edge g u v [weight 1])
      (let ([new-edge (edge u v weight)])
-       (graph (graph-nodes g) (cons new-edge (graph-edges g)))))
+       (my-graph (my-graph-nodes g) (cons new-edge (my-graph-edges g)))))
 
    (define (add-directed-edge g u v [weight 1])
      (let ([new-edge (edge u v weight #t)])
-       (graph (graph-nodes g) (cons new-edge (graph-edges g)))))
+       (my-graph (my-graph-nodes g) (cons new-edge (my-graph-edges g)))))
 
    (define (remove-edge g u v)
      (define (helper edges)
@@ -243,7 +277,7 @@
           (cdr edges)]
          [else
           (cons (car edges) (helper (cdr edges)))]))
-     (graph (graph-nodes g) (helper (graph-edges g))))
+     (my-graph (my-graph-nodes g) (helper (my-graph-edges g))))
 
    (define (remove-directed-edge g u v)
      (define (helper edges)
@@ -255,43 +289,18 @@
           (cdr edges)]
          [else
           (cons (car edges) (helper (cdr edges)))]))
-     (graph (graph-nodes g) (helper (graph-edges g))))]
-  #:mutable
-  #:transparent)
-
-(define find-node
-  (lambda (n g)
-    (define helper
-      (lambda (ns)
-        (cond
-          [(empty? ns)
-           (error 'find-node "could not find node")]
-          [(equal? (node-id n) (node-id (car ns)))
-           (car ns)]
-          [else
-           (helper (cdr ns))])))
-    (helper (graph-nodes g))))
-
-(define construct-graph
-  (lambda (nodes need-edge? make-edge)
-    (define helper
-      (lambda (node other-nodes)
-        (cond
-          [(empty? other-nodes)
-           '()]
-          [(need-edge? node (car other-nodes))
-           (cons (make-edge node (car other-nodes))
-                 (helper node (cdr other-nodes)))]
-          [else
-           (helper node (cdr other-nodes))])))
-    (define all-edges
-      (lambda (ns)
-        (if (empty? ns)
-            '()
-            (append (helper (car ns) (cdr ns))
-                    (all-edges (cdr ns))))))
-      (graph nodes (all-edges nodes))))
+     (my-graph (my-graph-nodes g) (helper (my-graph-edges g))))
 
 
+(define (write-graph-to-dot g)
+  (define f (open-output-file "out.dot" #:mode 'text #:exists'replace))
+  (define dot (graphviz g ))
+  (display dot f)
+  (close-output-port f))
+  
+;;(add-vertex! (my-graph '() '()) (node (gensym) 2))
+
+
+;; neato -T pdf 
 
 
